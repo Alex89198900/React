ba984376c5a1a46cfbe3826bbfb65c56d8aa498d
Alex89198900 <alexchorn1989@ukr.net>
2023-03-31 11:35:47 +0300

    feat: add func to router

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -4,19 +4,17 @@ import About from '../pages/Aboutus';
 import Notfound from '../pages/Notfoundpage';
 import Main from '../pages/Main';
 import Form from '../pages/Form';
-class Routeses extends React.Component {
-  render() {
-    return (
-      <div>
-        <Routes>
-          <Route path="/" element={<Main />} errorElement={<Notfound />} />
-          <Route path="/about" element={<About />} errorElement={<Notfound />} />
-          <Route path="/form" element={<Form />} errorElement={<Notfound />} />
-          <Route path="*" element={<Notfound />} />
-        </Routes>
-      </div>
-    );
-  }
+function Routeses() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Main />} errorElement={<Notfound />} />
+        <Route path="/about" element={<About />} errorElement={<Notfound />} />
+        <Route path="/form" element={<Form />} errorElement={<Notfound />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Routeses;
