9be8356d093c5f8f31e70eb3d4b147ce58f708db
Alex89198900 <alexchorn1989@ukr.net>
2023-03-31 13:06:54 +0300

    feat: add hook to form

---
+++ src/components/Formcomponent.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { FieldErrors, UseFormRegister } from 'react-hook-form';
+
+export type FormInputs = {
+  name?: string;
+  date?: string;
+  photo?: string;
+  frame?: string;
+  contactChoice?: string;
+  conf?: boolean;
+};
+type ComponentPropsd = {
+  register: UseFormRegister<FormInputs>;
+  errors: FieldErrors<FormInputs>;
+};
+
+export function MyInputFile(props: ComponentPropsd) {
+  return (
+    <div className="field">
+      <label htmlFor="photo">Photo</label>
+      <input
+        id="photo"
+        type="file"
+        {...props.register('photo', {
+          required: 'Please Choise Image',
+        })}
+      />
+      <div className="message-errr">{props.errors.photo?.message}</div>
+    </div>
+  );
+}
+export function MyInputText(props: ComponentPropsd) {
+  return (
+    <div className="field">
+      <label htmlFor="name">Name</label>
+      <input type="text" {...props.register('name')} id="name" />
+    </div>
+  );
+}
+export function MyInputDate(props: ComponentPropsd) {
+  return (
+    <div className="field">
+      <label htmlFor="date">Date</label>
+      <input
+        type="date"
+        id="date"
+        {...props.register('date', {
+          required: 'Please Choise Date',
+        })}
+      />
+      <div className="message-errr">{props.errors.date?.message}</div>
+    </div>
+  );
+}
+export function MySelect(props: ComponentPropsd) {
+  return (
+    <div className="field">
+      <label htmlFor="cars">framework</label>
+      <select {...props.register('frame', { required: 'Please Choise framework' })} id="cars">
+        <option value="DEFAULT" hidden></option>
+        <option value="React">React</option>
+        <option value="Angular">Angular</option>
+        <option value="Vue">Vue</option>
+        <option value="Svelte">Svelte</option>
+      </select>
+      <div className="message-errr">{props.errors.frame?.message}</div>
+    </div>
+  );
+}
+export function MyInputRadio(props: ComponentPropsd) {
+  return (
+    <div className="field">
+      <div>
+        <input
+          type="radio"
+          id="contactChoice1"
+          {...props.register('contactChoice', { required: 'Please Choise framework' })}
+          value="Javascript"
+        />
+        <label htmlFor="contactChoice1">Javascript</label>
+        <input
+          type="radio"
+          id="contactChoice2"
+          {...props.register('contactChoice')}
+          value="Typescript"
+        />
+        <label htmlFor="contactChoice2">Typescript</label>
+        <div className="message-errr">{props.errors.contactChoice?.message}</div>
+      </div>
+    </div>
+  );
+}
+
+export function MyInputCheckbox(props: ComponentPropsd) {
+  return (
+    <div className="field">
+      <label htmlFor="conf">confirm</label>
+      <input
+        type="checkbox"
+        id="conf"
+        {...props.register('conf', { required: 'Please confirm' })}
+      />
+      <div className="message-errr">{props.errors.conf?.message}</div>
+    </div>
+  );
+}
--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,7 +1,16 @@
-import React, { FormEvent } from 'react';
+import React from 'react';
 import Listform from '../components/Listforms';
 import Header from '../components/Header';
-
+import { FieldValues, useForm } from 'react-hook-form';
+import {
+  FormInputs,
+  MyInputFile,
+  MyInputText,
+  MyInputDate,
+  MySelect,
+  MyInputRadio,
+  MyInputCheckbox,
+} from '../components/Formcomponent';
 export interface CustomElementType {
   id: string;
   name: string | undefined;
@@ -11,191 +20,63 @@ export interface CustomElementType {
   conf: boolean | undefined;
   radio: string;
 }
-
-class Form extends React.Component {
-  private arr: Array<CustomElementType> = [];
-  name = React.createRef<HTMLInputElement>();
-  photo = React.createRef<HTMLInputElement>();
-  date = React.createRef<HTMLInputElement>();
-  frame = React.createRef<HTMLSelectElement>();
-  conf = React.createRef<HTMLInputElement>();
-  contactChoice1 = React.createRef<HTMLInputElement>();
-  contactChoice2 = React.createRef<HTMLInputElement>();
-  form = React.createRef<HTMLFormElement>();
-  elem = {
-    arr: this.arr,
-    mess: '',
-    err: {
-      id: '',
-      name: '',
-      photo: '',
-      date: '',
-      conf: '',
-      radio: '',
-      frame: '',
-    },
+let mess = '';
+const arrCards: CustomElementType[] = [];
+const preparationArr = (data: FieldValues): CustomElementType[] => {
+  const photoFile =
+    data.photo?.length !== 0 && data.photo ? URL.createObjectURL(data.photo[0]) : '';
+  const dataCard = {
+    id: String(arrCards.length),
+    name: data.name,
+    photo: photoFile,
+    date: data.date,
+    frame: data.frame,
+    conf: data.conf,
+    radio: data.contactChoice,
   };
+  arrCards.push(dataCard);
+  showMessage();
+  return arrCards;
+};
+function showMessage() {
+  mess = 'CARD CREATED';
+  setTimeout(() => {
+    mess = '';
+  }, 3000);
+}
 
-  showMessage() {
-    this.elem.mess = 'CARD CREATED';
-    setTimeout(() => {
-      this.elem.mess = '';
-      this.setState({
-        mess: '',
-      });
-    }, 3000);
-  }
-
-  onSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    const name = this.name.current?.value.trim();
-    const date = this.date.current?.value;
-    const frame = this.frame.current?.value;
-    const conf = this.conf.current?.checked;
-
-    const photoFile =
-      this.photo.current?.files?.length !== 0 && this.photo.current?.files
-        ? URL.createObjectURL(this.photo.current.files[0])
-        : '';
-    const choise = this.contactChoice1.current?.checked
-      ? this.contactChoice1.current?.value
-      : this.contactChoice2.current?.checked
-      ? this.contactChoice2.current?.value
-      : '';
-
-    const data = {
-      id: String(this.arr.length),
-      name: name,
-      photo: photoFile,
-      date: date,
-      frame: frame,
-      conf: conf,
-      radio: choise,
-    };
-    if (this.validate(data)) {
-      this.showMessage();
-      this.arr.push(data);
-      this.setState({
-        arr: this.arr,
-      });
-      this.form.current?.reset();
-    }
+function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>({
+    mode: 'onSubmit',
+    reValidateMode: 'onSubmit',
+  });
+  const [arr, setCards] = React.useState<CustomElementType[]>([]);
+  const onSubmit = (data: FieldValues): void => {
+    setCards(preparationArr(data));
   };
-
-  validate(data: CustomElementType) {
-    const input = data;
-    const name = data.name ?? '';
-    const errors = {
-      id: '',
-      name: '',
-      photo: '',
-      date: '',
-      conf: '',
-      radio: '',
-      frame: '',
-    };
-    let isValid = true;
-    if (input.photo === '') {
-      isValid = false;
-      errors.photo = 'Please choise your photo.';
-    }
-    if (name === '' || name.charAt(0).toUpperCase() !== name.charAt(0)) {
-      isValid = false;
-      errors.name = 'Must start with a capital letter and not change two letters.';
-    }
-    if (input.date === '') {
-      isValid = false;
-      errors.date = 'Please enter your date.';
-    }
-    if (input.conf === false) {
-      isValid = false;
-      errors.conf = 'Please confirm.';
-    }
-    if (input.radio === '') {
-      isValid = false;
-      errors.radio = 'Please choise language.';
-    }
-    if (input.frame === 'DEFAULT') {
-      isValid = false;
-      errors.frame = 'Please choise framework.';
-    }
-    this.elem.err = errors;
-    this.setState({
-      err: errors,
-    });
-    return isValid;
-  }
-  render() {
-    return (
-      <>
-        <Header title="Form" />
-        <div className="message">{this.elem.mess}</div>
-        <div className="all-forms">
-          <form className="form" onSubmit={this.onSubmit} ref={this.form}>
-            <div className="field">
-              <label htmlFor="name">Photo</label>
-              <input id="photo" type="file" ref={this.photo} />
-              <div className="message-errr">{this.elem.err.photo}</div>
-            </div>
-
-            <div className="field">
-              <label htmlFor="name">Name</label>
-              <input id="name" type="text" ref={this.name} />
-              <div className="message-errr">{this.elem.err.name}</div>
-            </div>
-            <div className="field">
-              <label htmlFor="date">Date</label>
-              <input type="date" id="date" ref={this.date} />
-              <div className="message-errr">{this.elem.err.date}</div>
-            </div>
-            <div className="field">
-              <label htmlFor="cars">framework</label>
-              <select ref={this.frame} id="cars" defaultValue={'DEFAULT'}>
-                <option value="DEFAULT" hidden></option>
-                <option value="React">React</option>
-                <option value="Angular">Angular</option>
-                <option value="Vue">Vue</option>
-                <option value="Svelte">Svelte</option>
-              </select>
-              <div className="message-errr">{this.elem.err.frame}</div>
-            </div>
-            <div className="field">
-              <div>
-                <input
-                  name="drone"
-                  type="radio"
-                  id="contactChoice1"
-                  ref={this.contactChoice1}
-                  value="Javascript"
-                />
-                <label htmlFor="contactChoice1">Javascript</label>
-                <input
-                  name="drone"
-                  type="radio"
-                  id="contactChoice1"
-                  ref={this.contactChoice2}
-                  value="Typescript"
-                />
-                <label htmlFor="contactChoice2">Typescript</label>
-                <div className="message-errr">{this.elem.err.radio}</div>
-              </div>
-            </div>
-
-            <div className="field">
-              <label htmlFor="conf">confirm</label>
-              <input type="checkbox" id="conf" ref={this.conf} />
-              <div className="message-errr">{this.elem.err.conf}</div>
-            </div>
-            <button type="submit" className="submit">
-              Submit
-            </button>
-          </form>
-        </div>
-        <div className="list-forms">
-          <Listform elem={this.elem} />
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <Header title="Form" />
+      <div className="all-forms">
+        <div className="message">{mess}</div>
+        <form onSubmit={handleSubmit(onSubmit)} className="form">
+          <MyInputFile register={register} errors={errors} />
+          <MyInputText register={register} errors={errors} />
+          <MyInputDate register={register} errors={errors} />
+          <MySelect register={register} errors={errors} />
+          <MyInputRadio register={register} errors={errors} />
+          <MyInputCheckbox register={register} errors={errors} />
+          <button type="submit" className="submit">
+            Submit
+          </button>
+        </form>
+        <Listform elem={arr} />
+      </div>
+    </>
+  );
 }
 export default Form;
